19286b6f871a51907a891ee082544b1a308c154a
8954sood <8954sood@naver.com>
2024-07-17 08:52:50 +0900

    feat: Set navigation

--- app/src/main/java/com/grow/nago/MainActivity.kt
+++ app/src/main/java/com/grow/nago/MainActivity.kt
@@ -10,19 +10,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.grow.nago.ui.theme.NagoTheme
+import com.grow.nago.ui.theme.White
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navHostController = rememberNavController()
             NagoTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = White
                 ) {
-                    Greeting("Android")
+                    NavHost(
+                        navController = navHostController, 
+                        startDestination = "test"
+                    ) {
+                        composable("test") {
+                            Greeting(name = "test")
+                        }
+                    }
                 }
             }
         }
