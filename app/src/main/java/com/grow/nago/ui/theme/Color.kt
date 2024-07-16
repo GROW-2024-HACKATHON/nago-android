5ff2f8629046544a8fafe7388f5818e71a9f6e64
8954sood <8954sood@naver.com>
2024-07-16 21:37:46 +0900

    feat: Set Design System Color

--- app/src/main/java/com/grow/nago/ui/theme/Color.kt
+++ app/src/main/java/com/grow/nago/ui/theme/Color.kt
@@ -2,10 +2,25 @@ package com.grow.nago.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Transparent = Color(0x00FFFFFF)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+
+val Gray100 = Color(0xFFF4F5F9)
+val Gray200 = Color(0xFFF1F1F1)
+val Gray300 = Color(0xFFE6E6E6)
+val Gray400 = Color(0xFFD1D1D1)
+val Gray500 = Color(0xFFAAAAAA)
+val Gray600 = Color(0xFF787878)
+val Gray700 = Color(0xFF333333)
+val Gray800 = Color(0xFF1D1D1D)
+
+
+val Orange100 = Color(0xFFFFEADC)
+val Orange200 = Color(0xFFFC8C41)
+val Orange300 = Color(0xFFFF6F0F)
